004c377e4c0318385d16ff6e64e53ac78901f89d
Jack Belshe <jackbelshe@Jacks-Laptop.local>
2024-11-13 16:32:30 -0800

    added more formatting, fixed bugs in audio and loading

--- spotify-web-player/src/App.js
+++ spotify-web-player/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import SongCard from './SongCard'
+import "./index.css" 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap';
@@ -11,24 +12,24 @@ function App() {
       <Container className="my-5">
         <Row className="justify-content-center">
           <Col xs={12} sm={6} md={3} className="p-2">
-            <div className="songcard-container">
+            <div className="songcard-container" id="songcard0">
               <SongCard></SongCard>
             </div>
           </Col>           
           <Col xs={12} sm={6} md={3} className="p-2">
-            <div className="songcard-container">
+            <div className="songcard-container" id="songcard1">
               <SongCard></SongCard>
             </div>
           </Col> 
         </Row> 
         <Row className="justify-content-center">
           <Col xs={12} sm={6} md={3} className="p-2">
-            <div className="songcard-container">
+            <div className="songcard-container" id="songcard2">
               <SongCard></SongCard>
             </div>
           </Col>           
           <Col xs={12} sm={6} md={3} className="p-2">
-            <div className="songcard-container">
+            <div className="songcard-container" id="songcard3">
               <SongCard></SongCard>
             </div>
           </Col> 
--- spotify-web-player/src/SongCard.jsx
+++ spotify-web-player/src/SongCard.jsx
@@ -1,55 +1,86 @@
-import React, { useState, useEffect } from "react";
-import GetNewSong from './GetNewSong';
+import React, { useState, useEffect, useRef } from "react";
 import './SongCard.css';
 
 const SongCard = ({ }) => {
 
-
+    const [isBlur, setBlur] = useState(true);
     const [song_data, setSongData] = useState(null);
     const [audio_data, setAudioData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true)
+    const initialLoad = useRef(false);
+
+    const toggleBlur = (isBlur) => {
+        setBlur(!isBlur);
+    };
 
 
     const requestNewSong = async () => {
         console.log("Requesting New Song /song/random")
         try {
-            const response = await fetch('/song/random'); 
+            setIsLoading(true)
+            const response = await fetch('/song/random');
             console.log(response)
             const data = await response.json();
             console.log(data)
-            handleReceivedData(data)
+            let image = data['album_image'][0].url;
+            let audio = data['song_preview'];
+            setSongData(image);
+            setAudioData(audio)
+            console.log("Updating Song_Data", image);
+        } catch (error) {
+            console.log("ERROR with /song/random : ", error);
+        }
+        finally {
+            console.log("Setting isLoading to false");
+            setIsLoading(false);
+        }
+    };
+
+    const handleButtonClick = async () => {
+        console.log("Button Clicked requesting /song/random")
+        try {
+            if (isBlur) {
+                toggleBlur(isBlur);
+            }
+            else {
+                requestNewSong();
+                toggleBlur(isBlur);
+            }
         } catch (error) {
             console.log("ERROR with /song/random : ", error)
         }
     };
 
-    const handleReceivedData = (newData) => {
-        let image = newData['album_image'][0].url;
-        let audio = newData['song_preview'];
-        setSongData(image);
-        setAudioData(audio)
-        console.log("Updating Song_Data", image);
-    }
-
     useEffect(() => {
-        requestNewSong();
-    }, [] );
+        if (!initialLoad.current) {
+            requestNewSong();
+            initialLoad.current = true;
+        }
+    }, []);
 
     return (
-        <div className="container d-flex flex-column justify-content-center align-items-center">
-            <div class="overlay d-flex flex-column justify-content-center align-items-center position-absolute w-100">
-                {audio_data && (
-                    <audio controls autoplay="Play">
-                        <source src={audio_data} type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>)}
-                <div class="button-container">
-                    <GetNewSong onDataReceived={handleReceivedData} />
-                </div>
-            </div>
-            <div class="img-container position-relative">
-                {song_data && <img src={song_data} className="blurred"/>}
-            </div>
-        </div>
+        <>
+            {(song_data && audio_data) ? (
+                <div className="container d-flex flex-column justify-content-center align-items-center">
+                    <div className="audio-container d-flex flex-column justify-content-center align-items-center position-absolute w-100">
+                        {audio_data && (
+                            <audio key={audio_data} controls >
+                                <source src={audio_data} type="audio/mpeg" />
+                                Your browser does not support the audio element.
+                            </audio>)}
+                        <div className="button-container">
+                            <button onClick={handleButtonClick}>Press Me!</button>
+                        </div>
+                    </div>
+                    <div className="img-container position-relative">
+                        {song_data && <img src={song_data} className={isBlur ? "blurred" : "clear"} />}
+                    </div>
+                </div>)
+                : (<div className="container d-flex flex-column justify-content-center align-items-center">
+                    LOADING
+                    </div>)
+            }
+        </>
     );
 };
 
