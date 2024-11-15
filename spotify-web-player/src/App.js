8c47df6e61bc79eb8fd1ae0817ba905e9f599c9c
Jack Belshe <jackbelshe@Jacks-Laptop.lan>
2024-11-14 17:33:40 -0800

    more formatting. Added text form for guesses

--- spotify-web-player/src/App.js
+++ spotify-web-player/src/App.js
@@ -3,9 +3,14 @@ import SongCard from './SongCard'
 import "./index.css" 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Button} from 'react-bootstrap';
 
 function App() {
+
+
+  const RefreshCards = () => {
+    
+  };
  
   return (
     <>
@@ -13,27 +18,32 @@ function App() {
         <Row className="justify-content-center">
           <Col xs={12} sm={6} md={3} className="p-2">
             <div className="songcard-container" id="songcard0">
-              <SongCard></SongCard>
+              <SongCard card_id={0}></SongCard>
             </div>
           </Col>           
           <Col xs={12} sm={6} md={3} className="p-2">
             <div className="songcard-container" id="songcard1">
-              <SongCard></SongCard>
+              <SongCard card_id={1}></SongCard>
             </div>
           </Col> 
         </Row> 
         <Row className="justify-content-center">
           <Col xs={12} sm={6} md={3} className="p-2">
             <div className="songcard-container" id="songcard2">
-              <SongCard></SongCard>
+              <SongCard card_id={2}></SongCard>
             </div>
           </Col>           
           <Col xs={12} sm={6} md={3} className="p-2">
             <div className="songcard-container" id="songcard3">
-              <SongCard></SongCard>
+              <SongCard card_id={3}></SongCard>
             </div>
           </Col> 
         </Row> 
+        <Row className='justify-content-center'>
+          <Col md={3} className='d-flex justify-content-center'>
+            <Button onClick={RefreshCards}>Refresh</Button>
+          </Col> 
+        </Row>
       </Container>
     </>
   );
--- spotify-web-player/src/SongCard.jsx
+++ spotify-web-player/src/SongCard.jsx
@@ -1,18 +1,69 @@
 import React, { useState, useEffect, useRef } from "react";
+import { Button, Col, Form, Row } from 'react-bootstrap'
 import './SongCard.css';
 
-const SongCard = ({ }) => {
+const SongCard = ({ card_id }) => {
+
+
+    /* TODO: 
+        -Add control monitor in App.js so only one song place at a time
+        -Add button on bottom that updates all four 
+        -Add text form for user input guess
+            -Create recommendation system
+        -Switch press me button to a guess button
+        
+        Long term design:
+        -Multiple guesses
+        -Guess years for bonuses
+    */
 
     const [isBlur, setBlur] = useState(true);
     const [song_data, setSongData] = useState(null);
     const [audio_data, setAudioData] = useState(null);
     const [isLoading, setIsLoading] = useState(true)
     const initialLoad = useRef(false);
-
+    const [myColor, setColor] = useState(null);
+    const [buttonStyle, setButtonStyle] = useState(null);
+    const [bandGuess, setBandGuess] = useState("");
     const toggleBlur = (isBlur) => {
         setBlur(!isBlur);
     };
 
+
+
+    useEffect(() => {
+        var temp_color = null;
+        switch (card_id) {
+            case 0:
+                temp_color = "#61dafb";
+                setColor("#61dafb");
+                break;
+            case 1:
+                temp_color = "red";
+                setColor("red");
+                break;
+            case 2:
+                temp_color = "orange"
+                setColor("green");
+                break;
+            case 3:
+                temp_color = "green";
+                setColor("orange");
+                break;
+            default:
+                setColor("");
+        }
+
+        var buttonStyle = {
+            backgroundColor: temp_color,
+            color: "black"
+        }
+        setButtonStyle(buttonStyle);
+        if (!initialLoad.current) {
+            requestNewSong();
+            initialLoad.current = true;
+        }
+    }, []);
 
     const requestNewSong = async () => {
         console.log("Requesting New Song /song/random")
@@ -51,34 +102,44 @@ const SongCard = ({ }) => {
         }
     };
 
-    useEffect(() => {
-        if (!initialLoad.current) {
-            requestNewSong();
-            initialLoad.current = true;
-        }
-    }, []);
+    const checkGuess = (event) => {
+        event.preventDefault();
+        console.log("Submitted Value:", bandGuess)
+    };
+
+    const handleChange = (event) => {
+        console.log("Updated Band Guess to: ", event.target.value);
+        setBandGuess(event.target.value);
+    };
 
     return (
         <>
             {(song_data && audio_data) ? (
-                <div className="container d-flex flex-column justify-content-center align-items-center">
-                    <div className="audio-container d-flex flex-column justify-content-center align-items-center position-absolute w-100">
+                <div className="container position-relative">
+                    <Row className="search-container d-flex flex-column justify-content-center align-items-center position-absolute w-100">
+                        <Form className="d-flex" onSubmit={checkGuess}>
+                            <Form.Group controlId="formBasicText">
+                                <Form.Control className="guess-input" type="text" placeholder="Type Guess Here . . ." value={bandGuess} onChange={handleChange} />
+                            </Form.Group>
+                            <div className="button-container">
+                                <Button variant="secondary" type="submit" onClick={handleButtonClick} style={buttonStyle}>Guess</Button>
+                            </div>
+                        </Form>
+                    </Row>
+                    <Row className="audio-container d-flex flex-column justify-content-center align-items-center position-absolute w-100">
                         {audio_data && (
-                            <audio key={audio_data} controls >
+                            <audio key={audio_data} controls>
                                 <source src={audio_data} type="audio/mpeg" />
                                 Your browser does not support the audio element.
                             </audio>)}
-                        <div className="button-container">
-                            <button onClick={handleButtonClick}>Press Me!</button>
-                        </div>
-                    </div>
+                    </Row>
                     <div className="img-container position-relative">
                         {song_data && <img src={song_data} className={isBlur ? "blurred" : "clear"} />}
                     </div>
                 </div>)
                 : (<div className="container d-flex flex-column justify-content-center align-items-center">
                     LOADING
-                    </div>)
+                </div>)
             }
         </>
     );
